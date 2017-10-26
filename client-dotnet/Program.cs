a0b1c8813c2263063b93cee5eb1562f524f4cd6b
Alexander Steen <ale.steen@hotmail.com>
2017-10-26 17:46:04 +0200

    Initial commit

---
+++ client-dotnet/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace client_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("server", 8080);
+            
+            NetworkStream stream = client.GetStream();
+
+            // Receive the TcpServer.response.
+
+            // Buffer to store the response bytes.
+            Byte[] data = new Byte[256];
+
+            // String to store the response ASCII representation.
+            String responseData = String.Empty;
+
+            // Read the first batch of the TcpServer response bytes.
+            Int32 bytes = stream.Read(data, 0, data.Length);
+            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+            Console.WriteLine(responseData);         
+
+            // Close everything.
+            stream.Close();         
+            client.Close();     
+        }
+    }
+}
---
+++ server-dotnet/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace server_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool quit = false;
+
+            TcpListener myListner = 
+                new TcpListener(IPAddress.Any, 8080);
+            myListner.Start();
+
+            while(!quit)
+            {
+                Socket mySocket = myListner.AcceptSocket();
+                Stream myStream = new NetworkStream(mySocket);
+                StreamWriter writer = new StreamWriter(myStream)
+                    {
+                        AutoFlush = true
+                    };
+                writer.WriteLine("Hello I'm a TCP server");
+                myStream.Close();
+                mySocket.Close();
+            }
+        }
+    }
+}
